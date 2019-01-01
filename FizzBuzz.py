b5a1fa7b5ebfc8286885f39b0c66ff922d2bca67
Umisyo <kusunokisouta@gmail.com>
2019-01-02 00:12:20 +0900

    first commit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,17 +2,15 @@ def main():
     i = 0
 
     while i < 1000:
-        Fizz = i % 3
-        Buzz = i % 5
 
-        if Fizz == 0 and Buzz != 0:
+        if i % 15 == 0:
+            print(str(i) + 'FizzBuzz')
+
+        elif i % 3 == 0:
             print(str(i) + 'Fizz')
 
-        elif Fizz != 0 and Buzz == 0:
+        elif i % 5 == 0:
             print(str(i) + 'Buzz')
-
-        elif Fizz == 0 and Buzz == 0:
-            print(str(i) + 'FizzBuzz')
 
         else:
             print(str(i))
